134a8ba375bf3be15a731a8e9d3b2d46885710b6
elqabasy <mahros.elqabasy@hotmail.com>
2023-12-11 14:04:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(void) {
+    bool Status = true;
+    while (Status){
+        /* code */
+    }
     return 0;
 }
